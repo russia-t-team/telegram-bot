6b4b1e4294da658e4e22d0ffe6e45746b1fa80fd
Denis Nasyrov <nasyrovd@gmail.com>
2022-04-10 14:24:03 +0500

    disabled some stickers

--- bot.py
+++ bot.py
@@ -142,12 +142,12 @@ def shklyar(update: Updater, context: CallbackContext):
 
 def main():
     my_bot = Updater(getenv("TOKEN"))
-    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(UA_PATTERN), ukraine), 0)
+    # my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(UA_PATTERN), ukraine), 0)
     my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(MAT_PATTERN), mat), 0)
-    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(RU_PATTERN), russia), 0)
-    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(BE_PATTERN), belorus), 0)
+    # my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(RU_PATTERN), russia), 0)
+    # my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(BE_PATTERN), belorus), 0)
     my_bot.dispatcher.add_handler(MessageHandler(Filters.regex(WEATHER_PATTERN), weather_bot), 0)
-    my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id={855480940}), shklyar), 1)
+    # my_bot.dispatcher.add_handler(MessageHandler(Filters.user(user_id={855480940}), shklyar), 1)
     my_bot.start_polling()
     my_bot.idle()
 
